ffb32af1b4a78b5c4eece01ca93d4a2bba235fa5
ylmzoguzhan <80750095+ylmzoguzhan@users.noreply.github.com>
2022-06-15 14:28:59 +0300

    repositories

--- Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistence.Repositories;
 
 namespace ETicaretAPI.Persistence
 {
@@ -16,6 +18,15 @@ namespace ETicaretAPI.Persistence
         {
 
             services.AddDbContext<ETicaretAPIDbContext>(options => options.UseSqlServer(Configurations.ConnectionString));
+            
+            services.AddScoped<ICustomerReadRepository,CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
         }
     }
 }
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProductReadRepository _productReadRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productReadRepository = productReadRepository;
+            _productWriteRepository = productWriteRepository;
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+           await _productWriteRepository.AddRangeAsync(new()
+            {
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Product 1",
+                    Price = 100,
+                    CreatedDate = DateTime.Now,
+                    Stock = 10
+                },
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Product 2",
+                    Price = 200,
+                    CreatedDate = DateTime.Now,
+                    Stock = 20
+                },
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Product 3",
+                    Price = 300,
+                    CreatedDate = DateTime.Now,
+                    Stock = 30
+                },
+            });
+           await _productWriteRepository.SaveAsync();
+        }
+    }
+}
